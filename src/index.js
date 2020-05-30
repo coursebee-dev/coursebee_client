c2a4efa09499d21d72439501df900606033845c5
njayman <najishm828282@gmail.com>
2020-05-31 02:40:17 +0600

    google analytics tracking id to env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-168052447-1');
+ReactGA.initialize(process.env.REACT_APP_NOT_GA_TRACKING_ID);
 
 
 ReactDOM.render(
