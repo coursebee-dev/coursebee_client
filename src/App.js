0a824f6f738919eaa26c9c9eb25560ffa4a87abf
njayman <najishm828282@gmail.com>
2020-06-04 08:03:01 +0600

    yet another seo fixes. More to come

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import setCurrentUser from "./actions/setUser";
@@ -9,31 +9,10 @@ import { Provider } from "react-redux";
 import store from "./store";
 import axios from "axios";
 
-
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/landing/Landing";
-import LandingMentor from "./components/landingMentor/LandingMentor";
-import LandingAdmin from "./components/landingAdmin/LandingAdmin";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import RegisterMentor from "./components/authMentor/RegisterMentor";
-import LoginMentor from "./components/authMentor/LoginMentor";
-import RegisterAdmin from "./components/authAdmin/RegisterAdmin";
-import LoginAdmin from "./components/authAdmin/LoginAdmin";
-import VerifyEmail from "./components/verifyEmail/VerifyEmail"
-import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
-import DashboardMentor from "./components/dashboardMentor/DashboardMentor";
-import DashboardAdmin from "./components/dashboardAdmin/DashboardAdmin";
-import Footer from "./components/layout/Footer";
-import About from "./components/about/About";
-import ComingSoon from "./components/comingSoon/ComingSoon";
 import ReactGA from 'react-ga';
 import {createBrowserHistory} from 'history'
-import LiveClass from "./components/liveClass/LiveClass";
-import Course from "./components/course/Course";
-import Training from "./components/training/Training";
 import {Helmet} from 'react-helmet';
+import PathRoute from "./router";
 
 //axios.defaults.baseURL="http://localhost:5000"
 axios.defaults.baseURL = "https://coursebee-server.herokuapp.com"
@@ -80,43 +59,20 @@ class App extends Component {
     return (
       <div className="App">
         <Helmet
-  title={seo.title}
-  meta={[
-    {
-      name: "description",
-      property: "og:description",
-      content: seo.description
-    },
-    { property: "og:title", content: seo.title },
-    { property: "og:url", content: seo.url },
-  ]}
-/>
+        title={seo.title}
+        meta={[
+          {
+            name: "description",
+            property: "og:description",
+            content: seo.description
+          },
+          { property: "og:title", content: seo.title },
+          { property: "og:url", content: seo.url },
+        ]}
+        />
       <Provider store={store}>
         <Router history={history}>
-          <Switch>
-          <React.Fragment>
-              <Navbar />
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/mentor" component={LandingMentor} />
-              <Route exact path="/admin" component={LandingAdmin} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/mentor/register" component={RegisterMentor} />
-              <Route exact path="/mentor/login" component={LoginMentor} />
-              <Route exact path="/admin/register" component={RegisterAdmin} />
-              <Route exact path="/admin/login" component={LoginAdmin} />
-              <Route exact path="/verifyEmail" component={VerifyEmail} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/comingSoon" component={ComingSoon} />
-              <Route exact path="/liveClassroom" component={LiveClass} />
-              <Route exact path="/course" component={Course} />
-              <Route exact path="/training" component={Training} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute exact path="/mentor/dashboard" component={DashboardMentor} />
-              <PrivateRoute exact path="/admin/dashboard" component={DashboardAdmin} />
-              <Footer />
-              </React.Fragment>
-          </Switch>
+              <PathRoute />
         </Router>
       </Provider>
       </div>
