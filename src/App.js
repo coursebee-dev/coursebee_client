f46c1cf59a08b6e79e2d1b78670632d20dd5bbf3
MdShihab <shihabshahriar@iut-dhaka.edu>
2020-06-07 19:03:15 +0600

    changed baseURL to staging server

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import { Helmet } from 'react-helmet';
 import PathRoute from "./router";
 
 //axios.defaults.baseURL = "http://localhost:5000"
-axios.defaults.baseURL = "https://coursebee-server.herokuapp.com"
+axios.defaults.baseURL = "https://coursebee-server-staging.herokuapp.com"
+//axios.defaults.baseURL = "https://coursebee-server.herokuapp.com"
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
   // Set auth token header auth
