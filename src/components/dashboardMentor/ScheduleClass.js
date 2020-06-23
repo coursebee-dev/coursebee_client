07e19e956a7da58c7ee42abb7d3aae1ddee9b3d6
njayman <najishm828282@gmail.com>
2020-06-23 16:24:35 +0600

    tried to add payment (1)

--- src/components/dashboardMentor/ScheduleClass.js
+++ src/components/dashboardMentor/ScheduleClass.js
@@ -25,21 +25,25 @@ export default function ScheduleClass() {
     return (
         <div className="container">
             <div className="row z-depth-1">
-                <div className="col s6 offset-s2">
-                    <Link to="/mentor/dashboard" className="btn-flat waves-effect orange">
-                        <i className="material-icons left">keyboard_backspace</i>Go Back
-                    </Link>
-                    <div className="schedule col s12" style={{ paddingLeft: "11.250px" }}>
-                        <h4>
-                            <b>Schedule Class</b> Below
-                        </h4>
-                    </div>
-                    <div className="row schedule">
-                        <div className="freepaid">
-                            <button className="btn-large" value="free" onClick={e=>setclass(e.target.value)}>Schedule a Free Class</button>
+                <div className="schedule">
+                    <div className="row">
+                        <div className="col s12">
+                            <Link to="/mentor/dashboard" className="btn-flat waves-effect orange">
+                                <i className="material-icons left">keyboard_backspace</i>Go Back
+                            </Link>
+                            <div className="schedule col s12" style={{ paddingLeft: "11.250px" }}>
+                                <h4>
+                                    <b>Schedule Class</b> Below
+                                </h4>
+                            </div>
                         </div>
-                        <div className="freepaid">
-                            <button className="btn-large" value="paid" onClick={e=>setclass(e.target.value)}>Schedule a Paid Class</button>
+                        <div className="col s12 class-button">
+                            <div className="freepaid">
+                                <button className="btn-large" value="free" onClick={e=>setclass(e.target.value)}>Free Class</button>
+                            </div>
+                            <div className="freepaid">
+                                <button className="btn-large" value="paid" onClick={e=>setclass(e.target.value)}>Paid Class</button>
+                            </div>
                         </div>
                     </div>
                 </div>
