dbc4af291ebd3d37b682db9dafd21e20a988ef18
MdShihab <shihabshahriar@iut-dhaka.edu>
2020-06-25 22:36:46 +0600

    tried to add pop up api minor changes

--- src/components/liveClass/LiveClassList.js
+++ src/components/liveClass/LiveClassList.js
@@ -25,38 +25,46 @@ class LiveClassList extends Component {
                 console.log(err)
             });
     }
-    onRegisterClick = e => {
+    onRegisterClick =(liveclasstype) => e => {
         const liveclassid = e.target.value
-        const liveclasstype = e.target.id
-        if(!this.props.auth.isAuthenticated || this.props.auth.user.type !== "student"){
-            M.toast({ html: "Please login as a student"})
+        //const liveclasstype = e.target.liveclasstype
+        //console.log(liveclassid)
+        if (!this.props.auth.isAuthenticated || this.props.auth.user.type !== "student") {
+            M.toast({ html: "Please login as a student" })
             return
         }
-        
-        if(liveclasstype==="Free"){
+
+        if (liveclasstype === "Free") {
             axios.post(`/api/registerliveclass/${this.props.auth.user.id}/${liveclassid}`)
-            .then(res => {
-                M.toast({ html: res.data.message })
-            })
-            .catch(err => {
-                M.toast({ html: "Server Error"})
-                console.log(err)
-            });
-        } else if(liveclasstype==="Paid") {
+                .then(res => {
+                    M.toast({ html: res.data.message })
+                })
+                .catch(err => {
+                    M.toast({ html: "Server Error" })
+                    console.log(err)
+                });
+        } else if (liveclasstype === "Paid") {
             axios.post(`/api/registerliveclass/${this.props.auth.user.id}/${liveclassid}`)
-            .then(res => {
-                console.log(res.data)
-                window.open(res.data.GatewayPageURL);
-            })
-            .catch(err => {
-                M.toast({ html: "Server Error"})
-                console.log(err)
-            });
+                .then(res => {
+                    console.log(res.data)
+                    window.location.href =res.data.data;
+                })
+                .catch(err => {
+                    M.toast({ html: "Server Error" })
+                    console.log(err)
+                });
         }
     }
     componentDidMount() {
-        this.getLiveClasses()
-        
+        this.getLiveClasses();
+        (function (window, document) {
+            var loader = function () {
+                var script = document.createElement("script"), tag = document.getElementsByTagName("script")[0];
+                script.src = "https://sandbox.sslcommerz.com/embed.min.js?" + Math.random().toString(36).substring(7);
+                tag.parentNode.insertBefore(script, tag);
+            };
+            window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+        })(window, document);
     }
     render() {
         const seo = {
@@ -68,22 +76,27 @@ class LiveClassList extends Component {
         };
         const liveClasses = this.state.liveClasses.map(liveClass => (
             <li className="collection-item" key={liveClass._id}>
-                {(() => {
-                    switch (liveClass.class_type) {
-                    case "Free":   return (
-                        <p className="secondary-content">
-                            <button value={liveClass._id} id={liveClass.class_type} onClick={this.onRegisterClick} className="btn btn-small waves-effect waves-light hoverable orange darken-1 black-text">Register for free</button>
-                        </p>
-                    );
-                    case "Paid": return (
-                        <p className="secondary-content">
-                            <button value={liveClass._id} id={liveClass.class_type} /*id="sslczPayBtn" postdata={paymentData} endpoint="https://coursebee-server-staging.herokuapp.com/api/payment" order={liveClass._id+Math.random().toString(36).substring(7)}*/ onClick={this.onRegisterClick} className="btn btn-small waves-effect waves-light hoverable orange darken-1 black-text">Register for ৳ {liveClass.price}</button>
-                        </p>
-                    );
-                    }
-                })()}
+                {liveClass.class_type === "Paid" ?
+                    <p className="secondary-content">
+                        <button
+                            id="sslczPayBtn"
+                            value={liveClass._id} liveclasstype={liveClass.class_type} 
+                            onClick={this.onRegisterClick(liveClass.class_type)}
+                            className="btn btn-small waves-effect waves-light hoverable orange darken-1 black-text">
+                            Register for ৳ {liveClass.price}
+                        </button>
+                    </p> :
+                    <p className="secondary-content">
+                        <button
+                            value={liveClass._id} liveclasstype={liveClass.class_type} 
+                            onClick={this.onRegisterClick(liveClass.class_type)}
+                            className="btn btn-small waves-effect waves-light hoverable orange darken-1 black-text">
+                            Register for free
+                        </button>
+                    </p>
+                }
                 <h6>Topic : {liveClass.topic}</h6>
-                <div dangerouslySetInnerHTML={{__html: liveClass.description}} />
+                <div dangerouslySetInnerHTML={{ __html: liveClass.description }} />
                 <p>Start Time: {new Date(liveClass.start_time).toLocaleDateString() + " " + new Date(liveClass.start_time).toLocaleTimeString()} </p>
                 <p>Duration : {liveClass.duration}</p>
                 <p>Type: {liveClass.class_type}</p>
@@ -104,9 +117,9 @@ class LiveClassList extends Component {
                     ]}
                 />
                 <h4 style={{ margin: "50px" }}>Scheduled Classes</h4>
-                <ul style={{width:"100%",textAlign: "left" }} className="collection">{liveClasses.reverse()}</ul>
+                <ul style={{ width: "100%", textAlign: "left" }} className="collection">{liveClasses.reverse()}</ul>
                 <Link style={{ margin: "40px" }} to="/" className="btn-flat waves-effect teal darken-1 white-text">
-                        <i className="material-icons left">keyboard_backspace</i>Go Back
+                    <i className="material-icons left">keyboard_backspace</i>Go Back
                 </Link>
             </div>
         )
