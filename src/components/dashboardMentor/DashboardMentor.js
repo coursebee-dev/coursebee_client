b72d10fbb29ecde9e0102f6bce48a14da3b43414
njayman <najishm828282@gmail.com>
2020-06-27 05:49:51 +0600

    switching my system

--- src/components/dashboardMentor/DashboardMentor.js
+++ src/components/dashboardMentor/DashboardMentor.js
@@ -7,12 +7,27 @@ import HeaderImg from "../layout/HeaderImg"
 import liveClass from '../../images/liveClass.jpg'
 import onlineCourse from '../../images/onlineCourse.jpg'
 import LiveClassMentor from './LiveClassMentor'
+import M from "materialize-css"
+
 class DashboardMentor extends Component {
   onLogoutClick = e => {
     e.preventDefault();
     this.props.history.push("/");
     this.props.logoutUser();
   };
+
+  componentDidMount() {
+    // const options = {
+    //     inDuration: 250,
+    //     outDuration: 250,
+    //     opacity: 0.5,
+    //     dismissible: false,
+    //     startingTop: "4%",
+    //     endingTop: "10%"
+    // };
+    M.Modal.init(this.Modal);
+}
+
   render() {
     const { user } = this.props.auth; return (
       <div>
@@ -28,6 +43,18 @@ class DashboardMentor extends Component {
                   <a href="https://www.facebook.com/coursebee.live" target="_blank" rel="noopener noreferrer">facebook page</a>.
               </p>
               </h4>
+              <button data-target="accountmodal" className="btn modal-trigger">Edit Your Account Information</button>
+              <div id="accountmodal" ref={Modal => {
+                        this.Modal = Modal;
+                    }} className="modal">
+                <div className="modal-fixed-header">
+                  <button style={{float: "right" }} className="modal-close btn-floating" onClick={this.openmodal}><i class="close material-icons">close</i></button>
+                </div>
+                <div className="modal-content">
+                  <h4>Modal Header</h4>
+                  <p>A bunch of text</p>
+                </div>
+              </div>
               {this.props.auth.user.adminVerify === false ?
                 <h5 className="red-text">Your account has been suspended. Your information is under review.
                 <br />To schedule live classes or submit online courses please send us an email</h5>
