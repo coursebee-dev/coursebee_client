7b6855b984f13b9b29858fe34f6992312a8cfa0a
njayman <najishm828282@gmail.com>
2020-07-02 00:57:32 +0600

    added course details

--- src/router.js
+++ src/router.js
@@ -39,14 +39,11 @@ import PostPayment from './components/payment/PostPayment';
 import ForgotPass from './components/forgotPass/ForgotPass';
 import ForgotPassMentor from './components/forgotPass/ForgotPassMentor';
 import ForgotPassAdmin from './components/forgotPass/ForgotPassAdmin';
-<<<<<<< HEAD
 import Terms from './components/LegalDocuments/Terms';
 import Privacy from './components/LegalDocuments/Privacy';
 import Disclaimer from './components/LegalDocuments/Disclaimer';
 import Contact from './components/contact/Contact';
-=======
 import ChangePass from './components/forgotPass/ChangePass';
->>>>>>> 3cf3bb50b14c0858cb1a3b91014ee107b07ea8da
 
 
 export default function PathRoute() {
@@ -55,7 +52,6 @@ export default function PathRoute() {
             <React.Fragment>
                 <Navbar />
                 <main className="no-padding">
-<<<<<<< HEAD
                     <Route exact path="/" component={Landing} />
                     <Route exact path="/mentor" component={LandingMentor} />
                     <Route exact path="/admin" component={LandingAdmin} />
@@ -68,6 +64,7 @@ export default function PathRoute() {
                     <Route exact path="/admin/register" component={RegisterAdmin} />
                     <Route exact path="/admin/login" component={LoginAdmin} />
                     <Route exact path="/admin/forgotpass" component={ForgotPassAdmin} />
+                    <Route exact path="/changepass/:token" component={ChangePass} />
                     <Route exact path="/verifyemail" component={VerifyEmail} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/liveclass" component={LiveClassList} />
@@ -92,40 +89,6 @@ export default function PathRoute() {
                     <PrivateRouteAdmin exact path="/admin/dashboard/viewmentor/:mentorid" component={ViewMentorDetail} />
                     <PrivateRouteAdmin exact path="/admin/dashboard/viewliveclass" component={ViewLiveClass} />
                     <PrivateRouteAdmin exact path="/admin/dashboard/viewstudent" component={ViewStudent} />
-=======
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/mentor" component={LandingMentor} />
-                <Route exact path="/admin" component={LandingAdmin} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/forgotpass" component={ForgotPass} />
-                <Route exact path="/mentor/register" component={RegisterMentor} />
-                <Route exact path="/mentor/login" component={LoginMentor} />
-                <Route exact path="/mentor/forgotpass" component={ForgotPassMentor} />
-                <Route exact path="/admin/register" component={RegisterAdmin} />
-                <Route exact path="/admin/login" component={LoginAdmin} />
-                <Route exact path="/admin/forgotpass" component={ForgotPassAdmin} />
-                <Route exact path="/changepass/:token" component={ChangePass} />
-                <Route exact path="/verifyemail" component={VerifyEmail} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/liveclass" component={LiveClassList} />
-                <Route exact path="/course" component={Course} />
-                <Route exact path="/training" component={Training} />
-                <Route exact path="/success" component={PostPayment} />
-                <Route exact path="/failed" component={PostPayment} />
-                <Route exact path="/cancel" component={PostPayment} />
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                <PrivateRoute exact path="/dashboard/myliveclass/:id" component={MyLiveClass} />
-                <PrivateRoute exact path="/dashboard/liveclassroom/:classid" component={LiveClassRoom} />
-                <PrivateRouteMentor exact path="/mentor/dashboard" component={DashboardMentor} />
-                <PrivateRouteMentor exact path="/mentor/dashboard/scheduleclass" component={ScheduleClass} />
-                <PrivateRouteMentor exact path="/mentor/dashboard/liveclassroom/:classid" component={LiveClassRoomMentor} />
-                <PrivateRouteAdmin exact path="/admin/dashboard" component={DashboardAdmin} />
-                <PrivateRouteAdmin exact path="/admin/dashboard/viewmentor" component={ViewMentor} />
-                <PrivateRouteAdmin exact path="/admin/dashboard/viewmentor/:mentorid" component={ViewMentorDetail} />
-                <PrivateRouteAdmin exact path="/admin/dashboard/viewliveclass" component={ViewLiveClass} />
-                <PrivateRouteAdmin exact path="/admin/dashboard/viewstudent" component={ViewStudent} />
->>>>>>> 3cf3bb50b14c0858cb1a3b91014ee107b07ea8da
                 </main>
                 <Footer />
             </React.Fragment>
