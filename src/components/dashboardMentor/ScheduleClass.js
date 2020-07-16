5c7df74a59dff2a82a3f7e548536a73913a9f0bc
njayman <najishm828282@gmail.com>
2020-07-17 02:51:03 +0600

    few modifications

--- src/components/dashboardMentor/ScheduleClass.js
+++ src/components/dashboardMentor/ScheduleClass.js
@@ -115,16 +115,16 @@ class ScheduleClass extends Component {
                                     initialValue="<p>Add a description</p>"
                                     init={{
                                         height: 500,
-                                        menubar: false,
+                                        menubar: 'edit insert format table tools help',
                                         plugins: [
-                                            'advlist autolink lists link image charmap print preview anchor',
+                                            ' autolink media lists link charmap print preview anchor',
                                             'searchreplace visualblocks code fullscreen',
-                                            'insertdatetime media table paste code help wordcount'
+                                            'insertdatetime table paste code wordcount'
                                         ],
                                         toolbar:
                                             'undo redo | formatselect | bold italic backcolor | \
                                             alignleft aligncenter alignright alignjustify | \
-                                            bullist numlist outdent indent | removeformat | help'
+                                            bullist numlist outdent indent | removeformat '
                                     }}
                                     onEditorChange={this.handleEditorChange}
                                 />
