bddfbb0eed60d505a8ba724e7c82d3ff5c2d4c8a
njayman <najishm828282@gmail.com>
2020-07-27 17:13:50 +0600

    test

--- src/actions/authActionMentor.js
+++ src/actions/authActionMentor.js
@@ -10,7 +10,7 @@ import setCurrentUser from "./setUser";
 // Register User
 export const registerMentor = (userData, history) => dispatch => {
     axios
-        .post("/api/mentor/register", userData)
+        .post("/api/mentor/register", qs.stringify(userData))
         .then(res => {
             console.log(res.data)
             history.push("/verifyemail", res.data)
