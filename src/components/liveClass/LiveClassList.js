97ff6336c059d2bec811272c3b248916f003535f
zahid0033 <zahidhossain0033@gmail.com>
2020-09-17 18:32:30 +0600

    list edit

--- src/components/liveClass/LiveClassList.js
+++ src/components/liveClass/LiveClassList.js
@@ -88,7 +88,7 @@ class LiveClassList extends Component {
                             <p><b>Start Date: </b>Coming Soon</p>
                             <p><b>Duration :</b> {liveClass.temp_duration}</p>
                             {/*<p><b>Duration :</b> {Math.round(liveClass.duration / 60)} hour {liveClass.duration % 60} minutes</p>*/}
-                            <p><b>Price:</b> {liveClass.price === 0 ? "Price will be published soon"  : liveClass.price + " taka" }</p>
+                            <p><b>Price:</b> {liveClass.price === 0 ? "Price will be published soon"  : liveClass.price + " BDT" }</p>
                         </div>
                         <div className="card-action">
                             <div className="row">
